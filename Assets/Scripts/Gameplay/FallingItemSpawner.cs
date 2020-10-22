8af513b063b73083cf83fc546c299e3d4660466e
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2020-10-21 20:20:40 -0500

    avance en los items

---
+++ Assets/Scripts/Gameplay/FallingItemSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingItemSpawner : MonoBehaviour
+{
+    private FallingItem[] fallingItems;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void PopulateItems()
+    {
+        Object[] items = Resources.LoadAll(GameConstants.FallingItemsResourcesPath);
+        fallingItems = new FallingItem[items.Length];
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/FallingItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "FallingItem", menuName = "ScriptableObjects/FallingItem", order = 1)]
+public class FallingItem : ScriptableObject
+{
+    public Sprite sprite;
+    public float fallingSpeed;
+    public int amountToCollect;
+    public float pickupWeight;
+}
