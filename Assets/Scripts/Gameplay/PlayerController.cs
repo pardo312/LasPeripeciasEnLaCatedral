1c568fd779271c93db94e9c3f4b909668be66d0b
kurei <tamaotamamura@gmail.com>
2020-10-21 22:41:33 -0500

    se agrego una escena donde contiene el movimiento de flechas (izquierda y derecha) del personaje Tolsa. Se creo una carpeta llamada Character, una imagen de Tolsa y un script para su movimiento

---
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3f;
+    private SpriteRenderer spriteRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            TolsaMove(false);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            TolsaMove(true);
+        }
+    }
+
+    private void TolsaMove(bool girarIzqDer)
+    {
+        if ((!girarIzqDer ? spriteRender.flipX : !spriteRender.flipX))
+        { //para que el personaje gire al otro lado
+            spriteRender.flipX = girarIzqDer;
+        }
+        transform.Translate(!girarIzqDer ? speed : -speed, 0, 0);
+        //aqui se llamara al triger de la animacion
+    }
+}
