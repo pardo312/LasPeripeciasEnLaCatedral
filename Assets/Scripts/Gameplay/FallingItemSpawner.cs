281d66eba90c3acec0076adb68890273d8f96ce7
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2020-10-22 16:18:22 -0500

    c

--- Assets/Scripts/Gameplay/FallingItemSpawner.cs
+++ Assets/Scripts/Gameplay/FallingItemSpawner.cs
@@ -8,12 +8,18 @@ public class FallingItemSpawner : MonoBehaviour
 
     private void Awake()
     {
-        
+        PopulateItems();
     }
 
     private void PopulateItems()
     {
-        Object[] items = Resources.LoadAll(GameConstants.FallingItemsResourcesPath);
+        Object[] items = Resources.LoadAll(GameConstants.FallingItemsResourcesPath, typeof(FallingItem));
         fallingItems = new FallingItem[items.Length];
+        for (int i = 0; i < items.Length; i++)
+        {
+            fallingItems[i] = items[i] as FallingItem;
+        }
     }
+
+
 }
