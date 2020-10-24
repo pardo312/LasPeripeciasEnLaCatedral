4db7442843885f529c8c8d830ac2757acbd123db
kurei <tamaotamamura@gmail.com>
2020-10-24 00:40:01 -0500

    modificacion al HUD. Agregando metodo para desaparecer los corazones

--- Assets/Scripts/ScriptableObjects/CounterItems.cs
+++ Assets/Scripts/ScriptableObjects/CounterItems.cs
@@ -5,17 +5,32 @@ using UnityEngine.UI;
 
 public class CounterItems : MonoBehaviour
 {
-	public Text txtCepillos;
-	private int totalC=0;
+	private GameObject corazon;
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+	
+	private void OnTriggerEnter2D(Collider2D collider){
+		  //collider.gameObject.tag == "piedra" //si tiene tag
+		if (collider.gameObject.name == "piedra") 
+        {
+			for(int i=1;i<4;i++){
+				corazon = GameObject.Find("corazon_" + i);
+				if(corazon!=null){
+					Debug.Log(corazon.name);
+					corazon.SetActive(false);
+					break;
+				}
+					
+			}
+		}
+	}
 }
