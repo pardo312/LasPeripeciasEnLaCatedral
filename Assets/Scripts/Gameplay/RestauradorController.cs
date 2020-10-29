91e0f75e73e2d01f4e60cc0bde79c63d143b3f34
kurei <tamaotamamura@gmail.com>
2020-10-29 12:55:56 -0600

    Se agrego una escena y un script para mover al personaje en el eje Y cuando se encuentra con una escalera. Se agrego una escalera de prueba

---
+++ Assets/Scripts/Gameplay/RestauradorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestauradorController : MonoBehaviour
+{
+    private SpriteRenderer spriteRender;
+    private static Rigidbody2D rigid;
+	
+	[HideInInspector]
+	public bool escalera = false;
+	
+	public float climbSpeed = 5f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(escalera)
+		{
+			rigid.velocity = Vector2.zero;
+			float inputY = Input.GetAxisRaw("Vertical"); 
+			rigid.AddForce(new Vector2 (rigid.velocity.x,inputY*climbSpeed));
+		}
+    }
+
+	private void OnTriggerEnter2D(Collider2D collider2D)
+	{
+		if (collider2D.tag == "Escalera")
+		{
+			escalera = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collider2D)
+	{
+		if (collider2D.tag == "Escalera")
+		{
+			escalera = false;
+		}
+	}
+}
