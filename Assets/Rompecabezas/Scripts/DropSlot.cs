c5bf4ee7a43214dda12dd74e6752b55163d2ac62
kurei <tamaotamamura@gmail.com>
2020-11-05 15:34:52 -0600

    Se creo un juego del rompecabezas con la estatua de la caridad

---
+++ Assets/Rompecabezas/Scripts/DropSlot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Text.RegularExpressions;
+
+public class DropSlot : MonoBehaviour, IDropHandler
+{
+    public GameObject item;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (item != null && item.transform.parent != transform)
+        {
+            item = null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        bool drop = false;
+        if (eventData.pointerDrag != null)
+        {
+            Image imagen = eventData.pointerDrag.GetComponent<Image>();
+            //Debug.Log("Nombre" + imagen.name);
+            Image slot = eventData.pointerEnter.GetComponent<Image>();
+            //Debug.Log("NombreSlot:" + slot.name);
+            drop = removeLetters(imagen.name) == removeLetters(slot.name);
+        }
+
+        if (!item && drop)
+        {
+            item = DragHandler.itemDragging;
+            item.transform.SetParent(transform);
+            item.transform.position = transform.position;
+        }
+    }
+
+    private string removeLetters(string cadena)
+    {
+        return Regex.Replace(cadena, "[^0-9.]", "");
+    }
+}
