0446b6ad56a4a199fa7bd5622ab32b623ddb72ba
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2020-11-13 02:07:47 -0500

    lvl1@

--- Assets/LvL1/Scripts/Palomino.cs
+++ Assets/LvL1/Scripts/Palomino.cs
@@ -1,18 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Palomino : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject palomino;
+    private bool inAnimation1;
+    private bool inAnimation2;
+
+    [SerializeField] private float spawnSecondsMin;
+    [SerializeField] private float spawnSecondsMax;
+
+    [SerializeField] private float durationMin;
+    [SerializeField] private float durationMax;
+
+    private CountdownTimer spawnTimer;
+    private CountdownTimer durationTimer;
+
+
     void Start()
     {
-        
+        palomino.SetActive(false);
+
+        spawnTimer = gameObject.AddComponent<CountdownTimer>();
+        spawnTimer.AddTimerFinishedListener(HandleSpawnTimerFinished);
+        durationTimer = gameObject.AddComponent<CountdownTimer>();
+        durationTimer.AddTimerFinishedListener(HandleDurationTimerFinished);
+        RunSpawnTimer();
+    }
+
+    private void RunSpawnTimer()
+    {
+        spawnTimer.Duration = Random.Range(spawnSecondsMin, spawnSecondsMax);
+        spawnTimer.Run();
+    }
+
+    private void HandleSpawnTimerFinished()
+    {
+        palomino.SetActive(true);
+        inAnimation1 = true;
+    }
+
+    private void RunDurationTimer()
+    {
+        durationTimer.Duration = Random.Range(durationMin, durationMax);
+        durationTimer.Run();
+    }
+
+    private void HandleDurationTimerFinished()
+    {
+        inAnimation2 = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (inAnimation1)
+        {
+            palomino.transform.position = Vector3.Lerp(palomino.transform.position, Vector3.zero, Time.deltaTime);
+
+            if (palomino.transform.position.magnitude <= 0.01f)
+            {
+                inAnimation1 = false;
+                RunDurationTimer();
+            }
+        }
+
+        if (inAnimation2)
+        {
+            palomino.transform.position = Vector3.Lerp(palomino.transform.position, new Vector3(0, 10, 0), Time.deltaTime);
+
+            if (palomino.transform.position.y >= 9.9f)
+            {
+                inAnimation2 = false;
+                palomino.SetActive(false);
+                RunSpawnTimer();
+            }
+        }
     }
 }
