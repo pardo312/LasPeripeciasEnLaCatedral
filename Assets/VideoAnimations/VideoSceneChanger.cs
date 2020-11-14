a339fb96c0c2a2838c78098d6f861691d230ff40
Jiufen <da.pardo@uniandes.edu.co>
2020-11-14 11:58:23 -0500

    Change videosanimation

--- Assets/VideoAnimations/VideoSceneChanger.cs
+++ Assets/VideoAnimations/VideoSceneChanger.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VideoSceneChanger : MonoBehaviour
 {
     [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private Button btn;
+    [SerializeField] private string nextVideoSceneName;
 
     private void Start()
     {
-        videoPlayer.loopPointReached += ClickButton;
+        if(nextVideoSceneName=="")
+            videoPlayer.loopPointReached += ClickButton;
+        else
+            videoPlayer.loopPointReached += nextPart;
     }
-
+    private void nextPart(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextVideoSceneName);
+    }
     private void ClickButton(VideoPlayer vp)
     {
         btn.onClick.Invoke();
